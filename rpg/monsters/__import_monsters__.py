76dc86b5c87e032b980225a309dd4dcc37f86188
thebmo <bmosier@gmail.com>
2015-01-08 13:56:55 -0500

    added an __init__ builder to import all monster subclasses

---
+++ rpg/monsters/__import_monsters__.py
@@ -0,0 +1,32 @@
+import os
+
+# deletes and rebuilds the init file for up to date
+# __all__ variable
+def main():
+    mods = os.listdir(os.getcwd())
+    monsters = []
+    ignore = ['pyc', 'monster', '__init__', '__import_monsters__']
+    # ignore = ['pyc', 'monster', '__']
+    init = '__init__.py'
+    for mod in mods:
+        m = mod.split('.')
+        if m[0] not in ignore and m[1] not in ignore:
+            monsters.append(mod.split('.')[0])
+
+    try:
+        os.remove(init)
+
+    except:
+        print 'File did not exist, creating file'
+        pass
+        
+    with open(init, 'w') as I:
+        all = '__all__ = %s\n' % monsters
+        I.write(all)
+
+
+    raw_input('finished')
+        
+
+if __name__ == '__main__':
+    main()
