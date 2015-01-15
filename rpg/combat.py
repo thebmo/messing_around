5b5baf13b0636c97379d93119fa1aa1d7f598e32
thebmo <bmosier@gmail.com>
2015-01-15 13:33:55 -0500

    added pseudo code for combat logic

--- rpg/combat.py
+++ rpg/combat.py
@@ -14,16 +14,43 @@ def start_combat(party):
     
     # initializes combat variables
     in_combat = True
-    monsters = random_encounter()
+    monsters = random_encounter(console=True)
     initiative = determine_initiative(party, monsters)
     # end combat initialization
-    
-    
 
+    # pseudo code below for basic combat logic
+    # monster/party death checks should be done in between
+    # combat action. combat should end if party has fallen
+    # or all monsters have been defeated
+    # while still in combat
+        # for loop through initiatives
+            # if member is dead:
+                # continue
+            
+            # check for available options
+                # attack
+                # defend
+                # spells
+                    # go throw hero spells
+                        # if rank >0: list spell
+                # if member in party (not monsters)
+                    # items
+                        # check for items owned by party
+        
+        # if any monsters still aliveL
+            # if monsters_are_dead(monsters):
+                # in_combat = False
+            # continue combat
+        # else:
+            # total exp gains
+            # for member in party:
+                # if not member is_dead:
+                    # give xp
+    # end pseudo code for combat logic
 
 
 # determines which monsters attack the party
-def random_encounter():
+def random_encounter(console=False):
     
     # this will bey dynamically extended
     monster_pool = ['Slime', 'Imp']
@@ -36,9 +63,10 @@ def random_encounter():
         
     monsters = fetch_monsters(monsters)
     
-    print'%s monster%s appear%s!' % (m_count, 's'[m_count==1:], 's'[m_count!=1:])
-    for monster in monsters:
-        print '%s: %s' % (monster.name, monster.hp)
+    if console:
+        print'%s monster%s appear%s!' % (m_count, 's'[m_count==1:], 's'[m_count!=1:])
+        for monster in monsters:
+            print '%s: %s' % (monster.NAME, monster.hp)
 
     return monsters
 
@@ -59,32 +87,34 @@ def fetch_monsters(monsters):
     return monster_objects
 
 
-# def combat_round(party, monsters, initiative):
-# def combat_round(party_monsters_initiative_dict):
-
-def determine_initiative(party, monsters):
+# calculates and returns a list of tuples containing
+# sorted monster and party initiatives
+# Ex:
+#   (party or monster, object list index, initiative roll)
+def determine_initiative(party, monsters, testing=False):
     
     # a list of tuples (p/m, index, roll)
     initiatives = []
     
     for i, p in enumerate(party):
-        # r = ad20 plus agility
+        # r = 1d20 plus agility
         r = randrange(0,20)+1 + p.stats['AGI']
         initiatives.append(('p', i, r))
     
     for i, m in enumerate(monsters):
-        r = randrange(0,20)+1
+        r = randrange(0,20)+1 + m.stats['AGI']
         initiatives.append(('m', i, r))
     
     # sorts initiative by roll value
     initiatives = sorted(initiatives, key=lambda x: x[2], reverse=True)
     
+    if testing:
+        print 'INITIATIVE ORDER'
+        print '================'
+        for i in initiatives:
+            print i
+
     return initiatives
-    
-    # print 'INITIATIVE ORDER'
-    # print '================'
-    # for i in initiatives:
-        # print i
 
 
 # checks if any monsters are alive
@@ -101,6 +131,18 @@ def monsters_are_dead(monsters):
             
     return all_dead
 
+
+# returns the amount of EXP for each party member
+def get_exp(monsters):
+    exp = 0
+    
+    for m in monsters:
+        exp += m.stats['exp']
+
+    return exp
+    
+    
+# main call for boilerplate code
 def main():
     random_encounter()
 
