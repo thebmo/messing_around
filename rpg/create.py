88db7e9578071d5ef92cc935f0a97e28dba8ab9a
thebmo <bmosier@gmail.com>
2015-01-18 18:24:14 -0500

    restricted hero names from allowing whitespace

--- rpg/create.py
+++ rpg/create.py
@@ -1,13 +1,17 @@
+from config import *
+
+
 # returns a single hero class object
 def create_hero():
+
     # main loop
     while(True):
         character_class = ''
         name = ''
 
         # asks for name input and varivies name < 8 Chars
-        while(len(name) <1 or len(name) > 8):
-            name =raw_input('Please enter hero name (8 or less characters): ')
+        while(len(name) <1 or len(name) > 8) or ' ' in name:
+            name =raw_input('Enter the hero\'s name (8 or less characters): ')
 
         # asks for class number and makes sure is valid choice
         while(character_class not in ['1','2','3','4']):
@@ -43,12 +47,14 @@ def create_party():
     creating = True
     party = []
 
+    print '\nplease create your party (max members= %s)' % MAX_PARTY
+    
     while(creating):
         
         party.append(create_hero())
 
         add_more = ' '
-        while(add_more[0] != 'y' and add_more[0] != 'n' and len(party) < 4):
+        while(add_more[0] != 'y' and add_more[0] != 'n' and len(party) < MAX_PARTY):
             add_more = raw_input('Add more party members (4 max)? (y/n)').lower()
         if add_more[0] == 'y':
             print '\n'
