22fc59f08793a26acd9927ba29db18c082384ce7
thebmo <bmosier@gmail.com>
2015-01-26 18:59:19 -0500

    fixed my is2 answer to use sets

---
+++ is2.py
@@ -0,0 +1,41 @@
+# 1) You are working at a magazine. Write a function that takes in two lists of customers, last month and this month,
+# renewals, cancelations, new subscriptions
+
+# https://wiki.python.org/moin/TimeComplexity
+
+# last_month = ['Conor', 'Paul', 'Brian']
+# this_month = ['Paul', 'Brian', 'Norbert']
+# return renewals, cancellations, new_subs
+
+
+# takes list arg (this/last month)
+def sub_changes(last_month, this_month):
+    sub_actions = []
+    
+    for last_sub in set(last_month):
+        if last_sub in this_month:
+            sub_actions.append((last_sub, 'renewal'))
+        else:
+            sub_actions.append((last_sub, 'cancel'))
+    
+    for this_sub in set(this_month):
+        if this_sub not in last_month:
+            sub_actions.append((this_sub, 'new subscriber'))
+
+    return sub_actions
+
+def main():
+    last_month = ['Conor', 'Paul', 'Brian']
+    this_month = ['Paul', 'Brian', 'Norbert']
+    
+    changes = sub_changes(last_month, this_month)
+    
+    print 'last month:\n', last_month
+    print 'this month:\n', this_month
+    print 'changes:\n', changes
+    print type(changes)
+    sett = set(changes)
+    print type(sett)
+# boiler code    
+if __name__ == '__main__':
+    main()
