5f13c0c857864bad57d55e9ede950d6c3eb7b03d
thebmo <bmosier@gmail.com>
2015-01-29 18:55:18 -0500

    refactored to explicitly call/label used combat_functions functions

--- rpg/combat.py
+++ rpg/combat.py
@@ -1,4 +1,4 @@
-from combat_functions import *
+import combat_functions as CF
 import combat_interface as CI
 from config import *                  # for global settings
 # from random import randrange, choice
@@ -13,8 +13,8 @@ def start_combat(Party, console=CONSOLE):
     
     # initializes combat variables
     in_combat = True
-    monsters = random_encounter(console=console)
-    initiatives = determine_initiative(party, monsters)
+    monsters = CF.random_encounter(console=console)
+    initiatives = CF.determine_initiative(party, monsters)
     # end combat initialization
 
     # pseudo code below for basic combat logic
@@ -23,12 +23,12 @@ def start_combat(Party, console=CONSOLE):
     # or all monsters have been defeated
 
     # pre-emptive fight type
-    pre_emp = pre_emptive()
+    pre_emp = CF.pre_emptive()
     
     if TESTING and pre_emp:
         print '\nPRE-EMPTIVE BATTLE!\n'
 
-    # ROUND LOOP
+    # START ROUND LOOP
     while (in_combat):
     
     # # MAKE A COMMAND LIST HERE
@@ -38,7 +38,7 @@ def start_combat(Party, console=CONSOLE):
         # target
         # ]
     
-        # TURN LOOP
+        # START TURN LOOP
         for initiative in initiatives:
             
             # skips monster turn if pre-emptive
@@ -52,21 +52,21 @@ def start_combat(Party, console=CONSOLE):
                     continue
             
             # sets member to the active character
-            member = get_member(initiative, party, monsters)
+            member = CF.get_member(initiative, party, monsters)
             
-            if party_is_dead(party):
+            if CF.party_is_dead(party):
                 print 'The party has been defeated.'
                 in_combat = False
                 break
             
             # ends combat if all monsters are dead
-            elif monsters_are_dead(monsters):
+            elif CF.monsters_are_dead(monsters):
                 print 'The party is victorious!'
                 in_combat = False
 
                 # total exp gains grants to live party
-                exp = get_exp(monsters)
-                gold = get_gold(monsters)
+                exp = CF.get_exp(monsters)
+                gold = CF.get_gold(monsters)
                 print 'The Party gains %s EXP and %s GOLD' % (exp, gold)
                 for p in party:
                     if not p.is_dead:
@@ -76,6 +76,8 @@ def start_combat(Party, console=CONSOLE):
                             if console:
                                 print '\n%s has leveled up' % p.name
                                 p.print_stats()
+                
+                # this breaks the turn for-loop
                 break
 
             # skips dead member's combat turn
@@ -97,7 +99,8 @@ def start_combat(Party, console=CONSOLE):
                     print '%s attacks %s!' % (member.name, target.name)
             
             
-            # END TURN LOOP
+            # END TURN FOR-LOOP
+
 
         # test block to end combat
         if TESTING:
@@ -109,4 +112,4 @@ def start_combat(Party, console=CONSOLE):
         # clears pre-emptive status
         pre_emp = 0
     
-    # END ROUND LOOP
+    # END ROUND WHILE-LOOP
