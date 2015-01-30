622611f2c9035904236e722ca96f52539114f9d9
thebmo <bmosier@gmail.com>
2015-01-29 19:51:12 -0500

    added some methods to party

--- rpg/heroes/party.py
+++ rpg/heroes/party.py
@@ -1,3 +1,9 @@
+"""
+    Party class that contains a list of all heroes
+    in the party as well as gold amount, items, and
+    Global Event Flags.
+"""
+
 class Party(object):
     
     def __init__(self, heroes, gold=0, items=[]):
@@ -5,8 +11,19 @@ class Party(object):
         if type(heroes) != list:
             raise Exception('Heroes not in list form')
             
-        self.members = heroes
+        self.heroes = heroes
         self.gold = gold
         self.items = items
-        
-    
+
+
+    # prints stats for entire party
+    def print_stats(self):
+        for h in self.heroes:
+            h.print_stats()
+
+
+    # Heals party to full HP/AP
+    def heal_party(self):
+        for h in self.heroes:
+            h.hp = h.stats['max_hp']
+            h.ap = h.stats['max_ap']
