1c8fba5d11bd27ac73b54e84db865a44f09f276c
thebmo <bmosier@gmail.com>
2015-02-25 18:24:49 -0500

    updated some cleaned up some comments

--- is2_othello.py
+++ is2_othello.py
@@ -21,12 +21,14 @@ def check_lines(coords, off_s, color, o_color):
     flip_these = []
 
     try:
+        # checks the rows to see if pieces should be flipped
         while(x+dx < len(game) and y+dy < len(game) and game[x+dx][y+dy] == o_color):
             x+=dx
             y+=dy
             
             flip_these.append((x, y))
 
+        # flips the pieces
         if game[x+(dx)][y+(dy)] == color:
             for f in flip_these:
                 game[f[0]][f[1]] = color
@@ -42,10 +44,8 @@ def main():
 
     coords = [3, 3] # the move the player makes
 
-
     dxs = (-1, 0, 1)
     dys = (-1, 0, 1)
-
 
     # prints starting game board
     print '\nBefore piece played'
@@ -61,7 +61,6 @@ def main():
             if dx == 0 and dy == 0:
                 continue
             check_lines(coords, (dx, dy), color, o_color)
-
 
     # prints teh finished game board
     print '\nAfter piece played'
