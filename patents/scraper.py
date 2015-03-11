77f22f3a29b130130edd3bcf942f7070d138b06d
thebmo <bmosier@gmail.com>
2015-03-11 16:20:50 -0400

    added a function with intent to make recursive

--- patents/scraper.py
+++ patents/scraper.py
@@ -2,36 +2,59 @@ import cPickle as pickle
 import urllib2
 from bs4 import BeautifulSoup
 
-P_FILE = 'patents.p'
+def main():
+    P_FILE = 'patents.p'
 
-SITE_URL = 'http://www.google.com/patents/sitemap/en/'
-current_url = SITE_URL + 'Sitemap.html'
-directory = { }
+    SITE_URL = 'http://www.google.com/patents/sitemap/en/'
+    current_url = SITE_URL + 'Sitemap.html'
+    directory = { }
 
-url = urllib2.urlopen(current_url)
-html = url.read()
-soup = BeautifulSoup(html)
+    url = urllib2.urlopen(current_url)
+    html = url.read()
+    soup = BeautifulSoup(html)
+    
+    black_list = {
+        'Google Home',
+        'USPTO Bulk Downloads',
+        'Privacy Policy',
+        'Terms of Service',
+        'About Google Patents',
+        'Send feedback',
+        }
+
+    for link in soup.find_all('a'):
+        if link.text in black_list:
+            continue
+        key = (str(link.text), link.get('href'))
+        directory[key] = get_next_dict(key, SITE_URL, black_list)
 
 
-for link in soup.find_all('a'):
-    key = (str(link.text), link.get('href'))
-    directory[key] = {}
+    pickle.dump(directory, open(P_FILE, 'wb'))
 
-for k in directory:
-    current_url = SITE_URL + str(k[1])
-    print current_url
+
+# takes a key, returns a dict
+def get_next_dict(old_key, url, black_list):
+    
+    new_dict = {}
+    new_url = ''.join((url, str(old_key[1])))
+    
     try:
-        url = urllib2.urlopen(current_url)
+        url = urllib2.urlopen(new_url)
         html = url.read()
         soup = BeautifulSoup(html)
         
         for link in soup.find_all('a'):
+            if link.text in black_list:
+                continue
             key = (link.text, link.get('href'))
-            directory[k][key] = {}
+            new_dict[key] = {}
+    
     except Exception as e:
-        print e
+        print e, ':', new_url
         pass
-        
+    
+    return new_dict
 
     
-pickle.dump(directory, open(P_FILE, 'wb'))
+if __name__ == '__main__':
+    main()
