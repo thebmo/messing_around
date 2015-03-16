1ffa57203c3c099e6ee53c593358b99e87e4e18b
thebmo <bmosier@gmail.com>
2015-03-16 19:00:11 -0400

    messing around to see if different methods yield same dict results

--- map_play.py
+++ map_play.py
@@ -1,20 +1,34 @@
+
 def main():
     nicks = {
-        'bmo': 'BMO',
-        'mike': 'Dickie',
-        'tim': 'tduds',
+        1: 'BMO',
+        2: 'Dickie',
+        3: 'tduds',
+        4: 'brendz84',
         }
-
+    
+ 
+    
+    # Using maps technique
     def nick_checker(key):
         return nicks[key]
+        
     nicks1 = map(nick_checker, nicks)
     
-    print nicks1, type(nicks)
-    print set(nicks1), type(nicks)
-    print set(nicks1), type(set(nicks))
     
-    nick_set = set(nicks1)
-    print nick_set
+    # Using For iteration
+    nicks2 = [nicks[k] for k in nicks]
+    
+    print 'nicks1:', nicks1
+    print 'nicks2:', nicks2
+    
+    
+    # print nicks1, type(nicks)
+    # print set(nicks1), type(nicks)
+    # print set(nicks1), type(set(nicks))
+    
+    # nick_set = set(nicks1)
+    # print nick_set
     
 
 if __name__ == '__main__':
