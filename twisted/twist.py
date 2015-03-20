f9c65fa854d05aa297850145c3c641a2ea667654
thebmo <bmosier@gmail.com>
2015-03-20 15:44:38 -0400

    playing with the call later reactor method

--- twisted/twist.py
+++ twisted/twist.py
@@ -1,12 +1,15 @@
-def notify():
-    print 'Reactor loop initiated!'
-    print 'Please continue to wait.'
+class Countdown(object):
+    counter = 5
+    
+    def count(self):
+        if self.counter == 0:
+            reactor.stop()
+        else:
+            print self.counter, '...'
+            self.counter -= 1
+            reactor.callLater(5, self.count)
 
 
 from twisted.internet import reactor
-
-reactor.callWhenRunning(notify)
-
-print 'Here we go!'
-
+reactor.callWhenRunning(Countdown().count)
 reactor.run()
