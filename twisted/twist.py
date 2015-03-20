a3e26a4c12232d041e616c015c49ac483408b1e4
thebmo <bmosier@gmail.com>
2015-03-20 15:22:19 -0400

    added  callwhenrunning method

--- twisted/twist.py
+++ twisted/twist.py
@@ -1,3 +1,12 @@
+def notify():
+    print 'Reactor loop initiated!'
+    print 'Please continue to wait.'
+
+
 from twisted.internet import reactor
+
+reactor.callWhenRunning(notify)
+
+print 'Here we go!'
 
 reactor.run()
