faecea2688e6af8f8cb96e89a741057f13f16f52
thebmo <bmosier@gmail.com>
2015-03-20 16:25:25 -0400

    updated to run multiple instances

--- twisted/twist.py
+++ twisted/twist.py
@@ -1,15 +1,20 @@
 class Countdown(object):
-    counter = 5
+    def __init__(self, name, counter=5, delay=1):
+        self.name = name
+        self.counter = counter
+        self.delay = delay
     
     def count(self):
         if self.counter == 0:
-            reactor.stop()
+            print self.name, ': FINSIHED'
         else:
-            print self.counter, '...'
+            print self.name, ':', self.counter, '...'
             self.counter -= 1
-            reactor.callLater(5, self.count)
+            reactor.callLater(self.delay, self.count)
 
 
 from twisted.internet import reactor
-reactor.callWhenRunning(Countdown().count)
+reactor.callWhenRunning(Countdown('1', counter=10, delay=1).count)
+reactor.callWhenRunning(Countdown('2', counter=5, delay=2).count)
+reactor.callWhenRunning(Countdown('3', counter=7, delay=1.5).count)
 reactor.run()
