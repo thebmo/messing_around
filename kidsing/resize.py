c3a1498c51127a632e27394d5bafd5d9cea90385
thebmo <bmosier@gmail.com>
2015-03-23 15:49:53 -0400

    resizes images to 700x400

---
+++ kidsing/resize.py
@@ -0,0 +1,18 @@
+import PIL
+
+
+# For each directory
+    # for each file
+        # resize each file to 700x465
+            # match ratio
+            # print file to different dir to make copying easy
+
+# takes image path as string
+def img_resize(img_path):
+    img = Image.open(img_path)
+    img.resize
+
+
+# boiler plate to start out
+if __name__ == '__main__':
+    main()
