9ecc0757b363bd53682174620f9bc6a7784a14d6
thebmo <bmosier@gmail.com>
2015-03-25 13:54:42 -0400

    added some formatting

--- args_n_kwargs.py
+++ args_n_kwargs.py
@@ -4,15 +4,17 @@ def print_args(*args):
         for a in args:
             print a
 
+            
 # Prints exactly 3 arguments
 def print_three(a, b, c):
     print a, b, c, 'as easy as 1 2 3'
+
     
 # Prints any number of kwargs
 def print_kwargs(**kwargs):
     if kwargs:
         for k in kwargs:
-            print '{}: {}'.format(k, kwargs[k])
+            print '{:>5} : {}'.format(k, kwargs[k])
 
     
 if __name__ == '__main__':
