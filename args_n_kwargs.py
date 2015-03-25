f93e42d258c69f340805c47e12cecaec2de6e501
thebmo <bmosier@gmail.com>
2015-03-25 13:48:07 -0400

    added a kwarg example

--- args_n_kwargs.py
+++ args_n_kwargs.py
@@ -8,9 +8,17 @@ def print_args(*args):
 def print_three(a, b, c):
     print a, b, c, 'as easy as 1 2 3'
     
+# Prints any number of kwargs
+def print_kwargs(**kwargs):
+    if kwargs:
+        for k in kwargs:
+            print '{}: {}'.format(k, kwargs[k])
+
     
 if __name__ == '__main__':
     myList = ['Bmo', 'Juju', 'Jared']
     print_args(myList, 'turkey', 'tofu')
     print ''
     print_three(*myList)
+    print ''
+    print_kwargs(list=myList, a='Bmo', b='Juju')
